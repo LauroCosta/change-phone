726439ac59012af99fca85f60a224b454d7eea4d
Lauro Costa <joaolauro94@gmail.com>
2021-08-04 00:22:51 -0300

    tela criada para editar conta

---
+++ src/pages/EditAccount/EditAccount.style.ts
@@ -0,0 +1,19 @@
+import { makeStyles } from "@material-ui/core";
+import {colors} from "../../config/theme"
+
+export const useStyles = makeStyles({
+  container: {
+    position: "relative",
+    background: colors.background,
+    height: "100vh",
+    overflow: "hidden",
+    padding: "0",
+  },
+
+
+  header: {
+
+    
+
+  }
+});
